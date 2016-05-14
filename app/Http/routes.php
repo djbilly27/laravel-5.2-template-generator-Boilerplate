3b78330e3c01b8080a5d337a437bb34fe7c38ca6
William Whitmire Mac Pro <b.whitmire@yourareacode.com>
2016-05-14 15:11:53 -0400

    first commit

---
+++ app/Http/routes.php
@@ -0,0 +1,73 @@
+<?php
+
+Route::group(['middleware' => 'web'], function() {
+    /**
+     * Switch between the included languages
+     */
+    Route::group(['namespace' => 'Language'], function () {
+        require (__DIR__ . '/Routes/Language/Language.php');
+    });
+
+    /**
+     * Frontend Routes
+     * Namespaces indicate folder structure
+     */
+    Route::group(['namespace' => 'Frontend'], function () {
+        require (__DIR__ . '/Routes/Frontend/Frontend.php');
+        require (__DIR__ . '/Routes/Frontend/Access.php');
+    });
+});
+
+/**
+ * Backend Routes
+ * Namespaces indicate folder structure
+ * Admin middleware groups web, auth, and routeNeedsPermission
+ */
+Route::group(['namespace' => 'Backend', 'prefix' => 'admin', 'middleware' => 'admin'], function () {
+    /**
+     * These routes need view-backend permission
+     * (good if you want to allow more than one group in the backend,
+     * then limit the backend features by different roles or permissions)
+     *
+     * Note: Administrator has all permissions so you do not have to specify the administrator role everywhere.
+     */
+    require (__DIR__ . '/Routes/Backend/Dashboard.php');
+    require (__DIR__ . '/Routes/Backend/Access.php');
+    require (__DIR__ . '/Routes/Backend/LogViewer.php');
+});
+
+
+/*
+|--------------------------------------------------------------------------
+| API routes
+|--------------------------------------------------------------------------
+*/
+
+Route::group(['prefix' => 'api', 'namespace' => 'API'], function () {
+    Route::group(['prefix' => 'v1'], function () {
+        require config('infyom.laravel_generator.path.api_routes');
+    });
+});
+
+
+Route::get('login', 'Auth\AuthController@getLogin');
+Route::post('login', 'Auth\AuthController@postLogin');
+Route::get('logout', 'Auth\AuthController@logout');
+
+// Registration Routes...
+Route::get('register', 'Auth\AuthController@getRegister');
+Route::post('register', 'Auth\AuthController@postRegister');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
+
+Route::get('/home', 'HomeController@index');
+
+Route::get('generator_builder', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@builder');
+
+Route::get('field_template', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@fieldTemplate');
+
+Route::post('generator_builder/generate', '\InfyOm\GeneratorBuilder\Controllers\GeneratorBuilderController@generate');
---
+++ config/infyom/generator_builder.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'views' => [
+
+        'builder' => 'vendor.infyom.generator-builder.builder',
+
+        'field-template' => 'vendor.infyom.generator-builder.field-template'
+    ]
+];
